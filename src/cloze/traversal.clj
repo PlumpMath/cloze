45853a6caf436e9f0aac47efeba44ca4df79ce8c
timsgardner <timsgardner@gmail.com>
2016-01-18 20:22:51 -0500

    changing leaf? to branch? in traversal

--- src/cloze/traversal.clj
+++ src/cloze/traversal.clj
@@ -3,41 +3,41 @@
 ;; ============================================================
 ;; basic traversals
 
-(defn prewalk [x f leaf? children make-node]
+(defn prewalk [x f branch? children make-node]
   (let [y (f x)]
-    (if (leaf? y)
+    (if (not (branch? y))
       y
       (make-node y
-        (map #(prewalk % f leaf? children make-node)
+        (map #(prewalk % f branch? children make-node)
           (children y))))))
 
-(defn prewalk-shallow [x p f leaf? children make-node]
+(defn prewalk-shallow [x p f branch? children make-node]
   (cond
     (p x) (f x)
-    (leaf? x) x
+    (not (branch? x)) x
     :else (make-node x
-            (map #(prewalk-shallow % p f leaf? children make-node)
+            (map #(prewalk-shallow % p f branch? children make-node)
               (children x)))))
 
-(defn postwalk [x f leaf? children make-node]
+(defn postwalk [x f branch? children make-node]
   (f
-    (if (leaf? x)
+    (if (not (branch? x))
       x
       (make-node x
-        (map #(postwalk % f leaf? children make-node)
+        (map #(postwalk % f branch? children make-node)
           (children x))))))
 
 ;; ============================================================
 ;; closures
 
-(defn prewalk-fn [leaf? children make-node]
+(defn prewalk-fn [branch? children make-node]
   (fn [x f]
-    (prewalk x f leaf? children make-node)))
+    (prewalk x f branch? children make-node)))
 
-(defn prewalk-shallow-fn [leaf? children make-node]
+(defn prewalk-shallow-fn [branch? children make-node]
   (fn [x p f]
-    (prewalk-shallow x p f leaf? children make-node)))
+    (prewalk-shallow x p f branch? children make-node)))
 
-(defn postwalk-fn [leaf? children make-node]
+(defn postwalk-fn [branch? children make-node]
   (fn [x f]
-    (postwalk x f leaf? children make-node)))
+    (postwalk x f branch? children make-node)))
